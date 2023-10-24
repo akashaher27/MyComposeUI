fda481cb730d9cd31d4cc29f96986031af8ae476
akashaher27 <akashaher27@gmail.com>
2023-10-24 09:37:04 +0530

    code Refactor

--- app/src/main/java/com/example/mycomposeui/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeui/MainActivity.kt
@@ -20,19 +20,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyComposeUITheme {
-        Greeting("Android")
     }
 }
